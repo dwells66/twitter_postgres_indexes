349ced9bb035ea5e882273da44b53e6a150ac549
dwells66 <dwells66@students.claremontmckenna.edu>
2026-05-08 01:28:10 -0700

    denormalized

--- sql.denormalized/01.sql
+++ sql.denormalized/01.sql
@@ -1,19 +1,10 @@
 /*
  * Count the number of tweets that use #coronavirus, seq scan since its one page read
  */
---CREATE INDEX ON tweets_jsonb USING gin(
-    --(data -> 'entities' -> 'hashtags') jsonb_path_ops);
-
---CREATE INDEX
---ON tweets_jsonb
---USING GIN ((data->'extended_tweet'->'entities'->'hashtags') jsonb_path_ops);
 
 SELECT COUNT(distinct data->>'id')
 FROM tweets_jsonb
 WHERE data->'entities'->'hashtags' @> '[{"text": "coronavirus"}]' 
     OR data->'extended_tweet'->'entities'->'hashtags' @> '[{"text": "coronavirus"}]';
 
--- jsonb array elements: set returning function, internally does a join
--- set returning functions cannot be sped up without an index
--- CREATE INDEX ON tweets_jsonb()
--- @N: contains
+
--- sql.denormalized/04.sql
+++ sql.denormalized/04.sql
@@ -2,13 +2,6 @@
  * Count the number of English tweets containing the word "coronavirus"
  */
 
---CREATE INDEX ON tweets_jsonb USING GIN (to_tsvector('english', data->>'text'));
-
---CREATE INDEX ON tweets_jsonb
---USING GIN (to_tsvector('english', data->'extended_tweet'->>'full_text'));
-
---CREATE INDEX ON tweets_jsonb ((data->>'lang')); 
-
 
 SELECT count(*)
 FROM tweets_jsonb
